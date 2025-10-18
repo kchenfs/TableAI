4018674398a46f19a08b3c39c43c2461e8c66e10
kchenfs <kendxd@gmail.com>
2025-10-18 13:02:49 -0400

    changed logiic

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,24 +1,2 @@
-# Define the IAM Role for unauthenticated Cognito users
-resource "aws_iam_role" "cognito_unauth_role" {
-  name = "TableAICognitoUnauthRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          "Federated" = "cognito-identity.amazonaws.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          "StringEquals" = {
-            "cognito-identity.amazonaws.com:aud" = aws_cognito_identity_pool.tableai_pool.id
-          }
-        }
-      }
-    ]
-  })
-}
 
 
